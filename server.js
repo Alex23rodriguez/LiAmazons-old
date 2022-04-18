13825313d95c97cd16f629b242891b34332b5745
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-04-18 02:00:40 -0500

    feat: ready to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const path = require("path");
 const app = express();
 
-const PORT = 5000;
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
