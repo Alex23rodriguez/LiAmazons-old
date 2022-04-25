a495bdc01d1b4c6712cf13ff0d8849e71538731c
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-04-25 18:13:36 -0500

    fix: changed baseURL in axios config to no longer use port 5000 everytime

--- server.js
+++ server.js
@@ -10,15 +10,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// const corsOptions = {
-//   origin: "*",
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ["Content-Type"],
-// };
-// app.use(cors(corsOptions));
-
 app.use(cors());
 app.use(express.json());
 
